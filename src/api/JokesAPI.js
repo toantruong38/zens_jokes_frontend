0e417ac6dc1d4db6262975ac5ac5addd8bfa41b3
toan-smartdev <toan.smartdev@gmail.com>
2023-02-26 10:10:04 +0700

    feat: update api endpoint

--- src/api/JokesAPI.js
+++ src/api/JokesAPI.js
@@ -1,6 +1,6 @@
 export function getJokes() {
   return fetch(
-    "http://ec2-54-95-91-196.ap-northeast-1.compute.amazonaws.com/api/jokes"
+    "http://ec2-54-248-36-183.ap-northeast-1.compute.amazonaws.com/api/jokes"
   )
     .then(async (res) => {
       if (res.status !== 200) {
@@ -15,7 +15,7 @@ export function getJokes() {
 
 export function voteJoke(id, vote) {
   return fetch(
-    "http://ec2-54-95-91-196.ap-northeast-1.compute.amazonaws.com/api/vote",
+    "http://ec2-54-248-36-183.ap-northeast-1.compute.amazonaws.com/api/vote",
     {
       body: JSON.stringify({ id, vote }),
       method: "POST",
